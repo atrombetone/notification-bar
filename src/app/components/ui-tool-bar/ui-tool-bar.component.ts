df30a47418986dfd5457fe7081f087d650128cdf
atrombetone <atrombetone@ciandt.com>
2020-04-22 22:09:06 -0300

    Teste de branch

---
+++ src/app/components/ui-tool-bar/ui-tool-bar.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-ui-tool-bar',
+  templateUrl: './ui-tool-bar.component.html',
+  styleUrls: ['./ui-tool-bar.component.scss']
+})
+export class UiToolBarComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
